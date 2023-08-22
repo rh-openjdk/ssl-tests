20951bab1a57d17fcb9040df0439052a487a7b8c
Zdenek Zambersky <zzambers@redhat.com>
2023-08-22 14:34:15 +0200

    ssl-tests-nss-client.sh: skip on Alpine Linux

--- jtreg-wrappers/ssl-tests-nss-client.sh
+++ jtreg-wrappers/ssl-tests-nss-client.sh
@@ -8,6 +8,11 @@
 set -eu
 rm -rf build
 
+if grep -q 'Alpine' /etc/os-release > /dev/null 2>&1 ; then
+    echo "Skipping on Alpine, as it does not have required NSS tools"
+    exit 0
+fi
+
 if ! type listsuites > /dev/null 2>&1 \
 && ! [ -e "/usr/lib64/nss/unsupported-tools/listsuites" ] \
 && ! [ -e "/usr/lib/nss/unsupported-tools/listsuites" ] ; then
